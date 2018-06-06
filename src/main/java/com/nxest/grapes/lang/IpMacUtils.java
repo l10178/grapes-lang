aadb21ba5c207440d639f473122ab62b08fafd50
l10178 <l10178@163.com>
2018-06-06 22:21:22 +0800

    init project

---
+++ src/main/java/com/nxest/grapes/lang/IpMacUtils.java
@@ -0,0 +1,9 @@
+package com.nxest.grapes.lang;
+
+/**
+ * IP and MAC util
+ *
+ * @author l10178
+ */
+public class IpMacUtils {
+}
---
+++ src/test/java/com/nxest/grapes/lang/IpMacUtilsTest.java
@@ -0,0 +1,10 @@
+package com.nxest.grapes.lang;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
+class IpMacUtilsTest {
+
+}
